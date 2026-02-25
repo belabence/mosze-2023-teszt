698a2fd13d57d78a1487d8929835ce750045a497
belabence <tothbelabence@gmail.com>
2026-02-25 15:27:20 +0100

    elso_merge

--- main.cpp
+++ main.cpp
@@ -1,29 +1,5 @@
 #include <iostream>
 
-<<<<<<< HEAD
-constexpr int N_ELEMENTS = 100;
-
-int main()
-{
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-=======
 constexpr int ELEMENTS = 100;
 
 int main()
@@ -47,6 +23,6 @@ int main()
     atlag /= ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     delete[] b;
->>>>>>> hibak
+
     return 0;
 }
