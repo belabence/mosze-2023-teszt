5b4ed444c39ea0629151ec9fcde7433dfa7b44c3
belabence <tothbelabence@gmail.com>
2026-02-25 15:15:32 +0100

    kommentek

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  //hianyzo _
+    std::cout << '1-100 ertekek duplazasa' // '' helyett ""
+    for (int i = 0;) // hianyzo < N_ELEMENTS és a i++
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // hianyzo < N_ELEMENTS
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hianyzo << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // ; problemak
     {
-        atlag += b[i]
+        atlag += b[i] // ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
+//eloszor elfelejtettem a kommenteket kiírni, azért van ez a branch.
